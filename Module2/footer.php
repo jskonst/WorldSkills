2b2ac3f77c8e682713e80bc423f0b09ca34b5986
deGraff-dev <61596920+deGraff-dev@users.noreply.github.com>
2022-02-20 15:31:16 +0300

    php blog
    your need create db for it

---
+++ Module2/footer.php
@@ -0,0 +1,6 @@
+<nav class="navbar navbar-dark bg-primary justify-content-between footer">
+                <p class="footer_text">
+                    Leonids site
+                </p>
+
+            </nav>
